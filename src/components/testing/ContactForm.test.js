9d18163066f1e8cd16557f5dc573b89f4116d7ee
Erica Girges <ericafiorenza@gmail.com>
2020-09-03 20:23:42 -0700

    pass with warning error.

--- src/components/testing/ContactForm.test.js
+++ src/components/testing/ContactForm.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import ContactForm from '../ContactForm';
-import { act } from 'react-dom/test-utils';
 
 test('Contact form accepts all input values and can be submitted', () => {
     render(<ContactForm />)
@@ -15,16 +14,20 @@ test('Contact form accepts all input values and can be submitted', () => {
     const message = screen.getByLabelText(/message*/i);
     expect(message).toBeTruthy();
 
-act(() => {
-    fireEvent.change(firstName, {target: {value: 'Tom'}});
+    fireEvent.change(firstName, {target: {value: 'Arnold'}});
     fireEvent.change(lastName, {target: {value: 'Schwarzenegger'}});
+    // breakMyTest();
     fireEvent.change(email, {target: {value: 'iamanemail@email.com'}});
     fireEvent.change(message, {target: {value: 'Hasta la vista, baby!'}});
-})
-
 
     const submitButton = screen.getByTestId(/submit/i);
     expect(submitButton).toBeTruthy();
     fireEvent.click(submitButton);
 
+
 })
+
+// test error
+const breakMyTest = () => {
+    throw new Error("I am an error")
+}
